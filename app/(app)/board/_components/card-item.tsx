7354cd15bbaca0940ab83705b1b973fa92adb727
itsarunkumar <003arunkumark@gmail.com>
2023-11-19 10:46:10 +0530

    mobile view card fix

--- app/(app)/board/_components/card-item.tsx
+++ app/(app)/board/_components/card-item.tsx
@@ -1,9 +1,19 @@
 "use client";
 
-import Modal from "@/components/modal";
 import { Card } from "@prisma/client";
 import React, { useEffect, useRef, useState } from "react";
 import { FileEdit } from "lucide-react";
+
+import Modal from "@/components/modal";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 import { updateCardData } from "@/actions/table-action";
 import { Input } from "@/components/ui/input";
@@ -43,69 +53,70 @@ export function CardItem({ card, tableId }: CardItemProps) {
   }
 
   return (
-    <div>
-      <div className="w-full" role="button" onClick={() => setShowModal(true)}>
-        {card.name}
-      </div>
-      <Modal
-        showModal={showModal}
-        setShowModal={() => {
-          setShowModal(false);
-          setIsEditing(false);
-        }}
-        className="w-96 min-h-[270px] shadow-sm border p-4"
-      >
-        <Toggle
-          className="absolute top-0 right-0 z-10"
-          onClick={() => setIsEditing(!isEditing)}
-        >
-          <FileEdit className="w-4 h-4" />
-        </Toggle>
-        <div className="relative  h-full w-full">
-          {isEditing ? (
-            <form
-              action={onSubmit}
-              className="w-full h-full flex flex-col gap-3 relative"
-            >
-              <input
-                type="text"
-                name="cardId"
-                id="cardId"
-                value={card.id}
-                hidden
-                readOnly
-              />
-              <Label htmlFor="card">Name</Label>
-              <Input
-                defaultValue={card.name}
-                name="card"
-                placeholder="card"
-                id="card"
-              />
-              <Label htmlFor="description">Description</Label>
-              <Textarea
-                defaultValue={card.description as string}
-                name="description"
-                placeholder="description"
-                id="description"
-              />
+    <Dialog>
+      <DialogTrigger className="w-full">{card.name}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{card.name}</DialogTitle>
+          <DialogDescription>
+            <div className="relative  h-full w-full">
+              {isEditing ? (
+                <form
+                  action={onSubmit}
+                  className="w-full h-full flex flex-col gap-3 relative"
+                >
+                  <input
+                    type="text"
+                    name="cardId"
+                    id="cardId"
+                    value={card.id}
+                    hidden
+                    readOnly
+                  />
+                  <Label htmlFor="card">Name</Label>
+                  <Input
+                    defaultValue={card.name}
+                    name="card"
+                    placeholder="card"
+                    id="card"
+                  />
+                  <Label htmlFor="description">Description</Label>
+                  <Textarea
+                    defaultValue={card.description as string}
+                    name="description"
+                    placeholder="description"
+                    id="description"
+                  />
 
-              <SubmitButton type="submit">save</SubmitButton>
-            </form>
-          ) : (
-            <div className="w-full h-full flex flex-col justify-between gap-3 ">
-              <h1 className="text-xl capitalize font-semibold">{card.name}</h1>
-              <div className="h-full text-slate-600">
-                <p>{card.description}</p>
-              </div>
+                  <SubmitButton type="submit">save</SubmitButton>
+                </form>
+              ) : (
+                <div className="w-full h-full flex flex-col justify-between gap-3 ">
+                  {/* <h1 className="text-xl capitalize font-semibold">
+                    {card.name}
+                  </h1> */}
+                  <div className="h-full text-slate-600">
+                    <p>{card.description}</p>
+                  </div>
 
-              <form action={deletesingleCard} className="w-full  ">
-                <SubmitButton className="w-full">Delete</SubmitButton>
-              </form>
+                  <form
+                    action={deletesingleCard}
+                    className="w-full flex items-center gap-2  "
+                  >
+                    <SubmitButton className="w-full">Delete</SubmitButton>
+                    <Toggle
+                      className=""
+                      onClick={() => setIsEditing(!isEditing)}
+                    >
+                      <FileEdit className="w-4 h-4" />
+                    </Toggle>
+                  </form>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </Modal>
-    </div>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
