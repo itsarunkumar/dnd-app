dbcb3159f43fe79d39138e2ad1e075042276b333
itsarunkumar <003arunkumark@gmail.com>
2023-11-22 14:58:02 +0530

    f

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import AuthNavbar from "./_components/navbar";
 
 async function AuthLayout({ children }: { children: React.ReactNode }) {
   const session = await auth();
 
   return (
     <div className="h-full flex flex-col justify-center items-center">
-      <AuthNavbar />
       {children}
     </div>
   );
